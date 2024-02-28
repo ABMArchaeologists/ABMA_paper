60626218b070bc9d83e5355a8609866b22e29be8
RonaldVisser <38214052+RonaldVisser@users.noreply.github.com>
2024-02-28 13:48:38 +0100

    update paper

--- Analyse_pre_workshop_survey.R
+++ Analyse_pre_workshop_survey.R
@@ -50,9 +50,15 @@ pre_surveys$What.is.your.age.[pre_surveys$What.is.your.age.=="Over 71 years"] <-
 # gender fill blanks
 pre_surveys$What.is.your.gender....Selected.Choice[pre_surveys$What.is.your.gender....Selected.Choice==""] <- "Not shared"
 
+pre_surveys$Which.software.do.you.know.for.Agent.Based.Modeling....Selected.Choice <- gsub("Other, please describe:", "Other", pre_surveys$Which.software.do.you.know.for.Agent.Based.Modeling....Selected.Choice)
+pre_surveys$Have.you.ever.applied.Agent.Based.Modelling.[pre_surveys$Have.you.ever.applied.Agent.Based.Modelling.==""] <- NA
+pre_surveys$Which.computer.skills.do.you.have....Selected.Choice <- gsub("Other, please describe:", "Other", pre_surveys$Which.computer.skills.do.you.have....Selected.Choice)
+pre_surveys$Which.of.the.option.below.applies.to.you.[pre_surveys$Which.of.the.option.below.applies.to.you.==""] <- "Not Shared"
+pre_surveys$Do.you.know.what.Agent.Based.Modeling.is.[pre_surveys$Do.you.know.what.Agent.Based.Modeling.is.==""] <- "Not shared"
+
 write.csv(pre_surveys, "data/pre_workshop_surveys.csv")
 saveRDS(pre_surveys, "data/pre_workshop_surveys.rds")
-readRDS("data/pre_workshop_surveys.rds")
+pre_surveys <- readRDS("data/pre_workshop_surveys.rds")
 
 # do you know ABM
 
@@ -61,8 +67,10 @@ ggplot(pre_surveys, aes(x = 1, fill = `Do.you.know.what.Agent.Based.Modeling.is.
   scale_fill_discrete(name = paste0("I know what ABM is (n=", sum(!is.na(pre_surveys$Do.you.know.what.Agent.Based.Modeling.is.)), ")"))
 ggsave("export/preworkshop/ABM_known_pie.png")
 
-ggplot(pre_surveys, aes(x = `Do.you.know.what.Agent.Based.Modeling.is.`)) +
-  geom_bar() + xlab("I know what ABM is") + facet_grid(~Workshop)
+pre_surveys %>%
+  filter(`Do.you.know.what.Agent.Based.Modeling.is.`!="Not shared") %>%
+  ggplot(aes(x = `Do.you.know.what.Agent.Based.Modeling.is.`)) +
+    geom_bar(na.rm = TRUE) + xlab("I know what ABM is") + facet_grid(~Workshop)
 ggsave("export/preworkshop/ABM_known_facet.png", width = 12)
 
 # Theory on ABM
@@ -90,7 +98,7 @@ ggsave("export/preworkshop/ABM_theory_quality_facet.png", width = 12)
 
 # ABM software
 
-pre_surveys$Which.software.do.you.know.for.Agent.Based.Modeling....Selected.Choice <- gsub("Other, please describe:", "Other", pre_surveys$Which.software.do.you.know.for.Agent.Based.Modeling....Selected.Choice)
+
 
 pre_surveys %>%
   select(`Which.software.do.you.know.for.Agent.Based.Modeling....Selected.Choice`) %>%
@@ -112,39 +120,43 @@ ggsave("export/preworkshop/ABM_software_knownfacet.png", width = 12)
 
 # ABM Applied
 
-pre_surveys$Have.you.ever.applied.Agent.Based.Modelling.[pre_surveys$Have.you.ever.applied.Agent.Based.Modelling.==""] <- NA
+
 
 ggplot(pre_surveys[!is.na(pre_surveys$Have.you.ever.applied.Agent.Based.Modelling.),], aes(x = 1, fill = `Have.you.ever.applied.Agent.Based.Modelling.`)) +
   geom_bar(na.rm = TRUE) + coord_polar(theta = "y", start=0) + theme_void() +
   scale_fill_discrete(name = paste0("Applied ABM before (n=", sum(!is.na(pre_surveys$Have.you.ever.applied.Agent.Based.Modelling.)), ")"))
-ggsave("export/preworkshop/gender_pie.png")
+ggsave("export/preworkshop/ABM_applied_before_pie.png")
 
-ggplot(pre_surveys[!is.na(pre_surveys$Have.you.ever.applied.Agent.Based.Modelling.),], aes(x = `Have.you.ever.applied.Agent.Based.Modelling.`)) +
-  geom_bar() + xlab("Applied ABM before")  +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
-  facet_grid(~Workshop)
+
+pre_surveys %>%
+  select(`Have.you.ever.applied.Agent.Based.Modelling.`, Workshop) %>%
+  filter(!is.na(`Have.you.ever.applied.Agent.Based.Modelling.`)) %>%
+  ggplot(aes(x = `Have.you.ever.applied.Agent.Based.Modelling.`)) +
+    geom_bar(na.rm = TRUE) + xlab("Applied ABM before")  +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+    facet_grid(~Workshop)
 ggsave("export/preworkshop/ABM_applied_before_facet.png", width = 12)
 
 # Computer skills
 
-pre_surveys$Which.computer.skills.do.you.have....Selected.Choice <- gsub("Other, please describe:", "Other", pre_surveys$Which.computer.skills.do.you.have....Selected.Choice)
-
 pre_surveys %>%
   select(`Which.computer.skills.do.you.have....Selected.Choice`) %>%
   separate_rows(`Which.computer.skills.do.you.have....Selected.Choice`, sep = ',\\s*') %>%
+  filter(`Which.computer.skills.do.you.have....Selected.Choice`!="") %>%
   group_by(`Which.computer.skills.do.you.have....Selected.Choice`) %>%
   ggplot(aes(x=`Which.computer.skills.do.you.have....Selected.Choice`)) + geom_bar() +
-  xlab("Computer skills") + coord_flip()
-
+    xlab("Computer skills") + coord_flip()
 ggsave("export/preworkshop/computer_skills.png")
 
 pre_surveys %>%
   select(`Which.computer.skills.do.you.have....Selected.Choice`, Workshop) %>%
   separate_rows(`Which.computer.skills.do.you.have....Selected.Choice`, sep = ',\\s*') %>%
+  filter(`Which.computer.skills.do.you.have....Selected.Choice`!="") %>%
   group_by(`Which.computer.skills.do.you.have....Selected.Choice`) %>%
   ggplot(aes(x=`Which.computer.skills.do.you.have....Selected.Choice`)) + geom_bar() +
   xlab("Computer skills") + coord_flip() + facet_grid(~Workshop)
 ggsave("export/preworkshop/computer_skills_facet.png", width = 12)
+
 
 
 # Gender
@@ -174,7 +186,7 @@ ggsave("export/preworkshop/age_classes_facet.png", width = 12)
 
 # age and gender
 ggplot(pre_surveys, aes(x = `What.is.your.age.`, fill = `What.is.your.gender....Selected.Choice`)) +
-  geom_bar() + xlab("Age") + facet_grid(~Workshop) +
+  geom_bar() + labs(x= "Age", y="Count", fill = "Gender") + facet_grid(~Workshop) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave("export/preworkshop/age_classes_gender_facet.png", width = 12)
 
@@ -193,7 +205,6 @@ ggsave("export/preworkshop/nationality_facet.png", width = 12)
 
 # student
 
-pre_surveys$Which.of.the.option.below.applies.to.you.[pre_surveys$Which.of.the.option.below.applies.to.you.==""] <- "Not Shared"
 ggplot(pre_surveys, aes(x = 1, fill = `Which.of.the.option.below.applies.to.you.`)) +
   geom_bar() + coord_polar(theta = "y", start=0) + theme_void() +
   scale_fill_discrete(name = paste0("Student or working (n=", sum(!is.na(pre_surveys$Which.of.the.option.below.applies.to.you.)), ")"))
